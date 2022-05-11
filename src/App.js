58e845209b6bbd7c5fd4a96c3d1ebf3830d85008
Felicitas Peredo <felicitasperedo97@gmail.com>
2022-05-11 20:46:24 -0300

    archivos no necesarios eliminados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NewComponent from './NewComponent';
+import Navbar from './NavBar';
 
 function App() {
   return (
     <div className="App">
       <header>
+        <Navbar></Navbar>
         <h1>Mi primera aplicacion React</h1>
       </header>
       <NewComponent></NewComponent>
