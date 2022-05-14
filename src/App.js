20664d450dc50fa3381ddb2e6cfacfbc0cbc370a
Felicitas Peredo <felicitasperedo97@gmail.com>
2022-05-14 18:13:53 -0300

    CartWidget / ItemListContainer  / Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <ItemListContainer></ItemListContainer>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemListContainer() {
+  return (
+    <div>
+        Catalogo de Productos
+    </div>
+  )
+}
+
+export default ItemListContainer
