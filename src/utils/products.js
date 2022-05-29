fd23596861ac56981cb30636d63072d5ae2a28a1
Felicitas Peredo <felicitasperedo97@gmail.com>
2022-05-29 18:00:16 -0300

    Descripcion de productos

--- src/utils/products.js
+++ src/utils/products.js
@@ -18,7 +18,7 @@ const products = [{
     id: "3",
     name: "Aros Oceania",
     categoria: "Aros",
-    description: "Oro",
+    description: "Argollas de brillantes bañadas en Plata",
     cost: 3000,
     image: "https://i.ibb.co/znzZHJy/Aros-Brillos.jpg",
     stock: 50
@@ -58,7 +58,7 @@ const products = [{
     id: "8",
     name: "Aros Nova",
     categoria: "Aros",
-    description: "Oro",
+    description: "Aros bañados en Oro con colgante de Perla",
     cost: 3000,
     image: "https://i.ibb.co/NZ2ZnSz/Aros-Perlita.jpg",
     stock: 50
@@ -75,7 +75,7 @@ const products = [{
     id: "10",
     name: "Anillo Egeo",
     categoria: "Anillos",
-    description: "Oro",
+    description: "Anillo con brillantes bañado en Oro",
     cost: 2500,
     image: "https://i.ibb.co/pbV2b7d/Anillo-Brillos-Oro.jpg",
     stock: 50
@@ -83,7 +83,7 @@ const products = [{
     id: "11",
     name: "Anillo Artico",
     categoria: "Anillos",
-    description: "Oro",
+    description: "Anillo bañado en Oro",
     cost: 2000,
     image: "https://i.ibb.co/XzZWqfK/Anillo-Gaia-Oro.jpg", 
     stock: 50 
@@ -91,7 +91,7 @@ const products = [{
     id: "12",
     name: "Anillo Gaia",
     categoria: "Anillos",
-    description: "Plata",
+    description: "Anillo en forma de cadena bañado en Oro",
     cost: 2300,
     image: "https://i.ibb.co/wp5pzSz/Anillo-Runa-Oro.jpg", 
     stock: 50 
@@ -100,7 +100,7 @@ const products = [{
     id: "13",
     name: "Collar Asia",
     categoria: "Collares",
-    description: "Oro",
+    description: "Collar bañado en Oro",
     cost: 4200,
     image: "https://i.ibb.co/s1W7Qwt/Collar-Circulo-Oro.jpg",
     stock: 50
@@ -108,7 +108,7 @@ const products = [{
     id: "14",
     name: "Collar Indica",
     categoria: "Collares",
-    description: "Oro",
+    description: "Collar grueso bañado en Oro",
     cost: 4000,
     image: "https://i.ibb.co/QK4mzK3/Collar-Grueso-Oro.jpg",
     stock: 50
@@ -116,7 +116,7 @@ const products = [{
     id: "15",
     name: "Collar Indica",
     categoria: "Collares",
-    description: "Plata",
+    description: "Collar grueso bañado en Plata",
     cost: 4000,
     image: "https://i.ibb.co/Fb9vpNs/Collar-Grueso-Plata.jpg",
     stock: 50
@@ -124,7 +124,7 @@ const products = [{
     id: "16",
     name: "Collar Amora",
     categoria: "Collares",
-    description: "Plata",
+    description: "Collar en forma de corazon partido bañado en Plata",
     cost: 3500,
     image: "https://i.ibb.co/tZdHnNM/Collar-Corazon-Plata.jpg",
     stock: 50
@@ -132,7 +132,7 @@ const products = [{
     id: "17",
     name: "Collar Oporto",
     categoria: "Collares",
-    description: "Plata",
+    description: "Collar con piedra bañado en Plata",
     cost: 3800,
     image: "https://i.ibb.co/4gxSCRC/Collar-Cuadrado-Plata.jpg",
     stock: 50
@@ -140,7 +140,7 @@ const products = [{
     id: "18",
     name: "Collar Lisboa",
     categoria: "Collares",
-    description: "Oro",
+    description: "Collar con dije bañado en Oro",
     cost: 3800,
     image: "https://i.ibb.co/5W27LSG/Collar-Dije-Oro.jpg",
     stock: 50
@@ -148,7 +148,7 @@ const products = [{
     id: "19",
     name: "Collar Laos",
     categoria: "Collares",
-    description: "Oro",
+    description: "Collar con dije de Perla bañado en Oro",
     cost: 4000,
     image: "https://i.ibb.co/HYpfpMC/Collar-Dije-Perla.jpg",
     stock: 50
@@ -156,7 +156,7 @@ const products = [{
     id: "20",
     name: "Collar Serp",
     categoria: "Collares",
-    description: "Oro",
+    description: "Collar cadena de serpiente bañado en Oro",
     cost: 3500,
     image: "https://i.ibb.co/tzD9QXC/Collar-Serpiente-Oro.jpg",
     stock: 50
@@ -164,7 +164,7 @@ const products = [{
     id: "21",
     name: "Collar Sintra",
     categoria: "Collares",
-    description: "Plata",
+    description: "Collar de cadena fina bañado en Plata",
     cost: 3000,
     image: "https://i.ibb.co/3rwpW3b/Collar-Mini-Plata.jpg",
     stock: 50
